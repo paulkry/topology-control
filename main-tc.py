3d1cb341caf4666d36b4573fd6d3bddd9d9698b8
shree <singhishree@gmail.com>
2025-07-15 21:18:45 +0530

    add volume compute from sdf

--- main-tc.py
+++ main-tc.py
@@ -29,7 +29,7 @@ for mesh_file in meshes:
 	random_points = np.random.uniform(-1, 1, (1000, 3))
 
 	# compute the signed distances at these points
-	d = igl.signed_distance( random_points, V, F )[0]
+	d = igl.blue_noise( random_points, V, F )[0]
 
 	# make a point cloud of the random points
 	point_cloud = ps.register_point_cloud( name + "sample points", random_points)
@@ -37,7 +37,7 @@ for mesh_file in meshes:
 	point_cloud.add_scalar_quantity( "signed_distance", d )
 
 	# sampling points on surface is important for learning.
-	# compute a bunch of blue noise points on the surface... something like  igl.sampling.poisson_disk(V, F, 1000, 0.1)  ??
+	# compute a bunch of blue noise points on the surface... something like
 	# compute a bunch of gaussian clouds around those surface points to get points inside and outside the mesh
 	# ant then perhaps save all these points to an npy file for faster loadding next time!
 	
