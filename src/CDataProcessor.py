e964d8a281a9a623000c49bcd8d9888aac778891
Ualibek Nurgulan <uali.nurgulan@gmail.com>
2025-07-16 03:26:50 +0700

    added Dataset class for Volume Regression Network

--- src/CDataProcessor.py
+++ src/CDataProcessor.py
@@ -1,6 +1,11 @@
 """
 [To be Completed] Generate point cloud pipeline for classifier for training and inference
 """
+
+from torch.utils.data import Dataset
+import torch
+import numpy as np
+
 class CDataProcessor:
     def __init__(self, config):
         """
@@ -39,3 +44,30 @@ class PointCloudPipeline:
         """
         
         return None
+
+"""
+Dataset Class for training network
+mapping latent vectors to volumes
+"""
+class VolumeDataset(Dataset):
+
+    def __init__(self, dataset_path):
+        """
+            Expecting a npz file with two arrays:
+            latents and volumes
+        """
+        self.file_path = dataset_path
+        data = np.load(dataset_path)
+        self.latents = torch.tensor(data["latents"], dtype=torch.float32)
+        self.volumes = torch.tensor(data["volumes"], dtype=torch.float32)
+        self.size = self.latents.shape[0]
+
+    def __getitem__(self, idx):
+        return self.latents[idx], self.volumes[idx]
+
+    def collate_fn(self, batch):
+        latents, volumes = zip(*batch)
+        return torch.stack(latents), torch.stack(volumes)
+
+    def __len__(self):
+        return self.size
