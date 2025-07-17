9aee2acb8ecf4472804ec0d8ba56cbe580459786
Ualibek Nurgulan <uali.nurgulan@gmail.com>
2025-07-18 02:18:52 +0700

    Rename trained model, refine path handling, add visualization for interpolation path

--- volume/config.py
+++ volume/config.py
@@ -1,7 +1,10 @@
 import torch
+import os
 
+current_file_path = os.path.abspath(__file__) # Absolute path to the current file
+VOLUME_DIR = os.path.dirname(current_file_path) # Directory containing the file
 LATENT_DIM = 2
-BATCH_SIZE = 8  # maybe reduce for CPU
+BATCH_SIZE = 8
 LR = 1e-3  # scale depending on batch
 DEV = torch.device("cpu")#torch.device("cuda" if torch.cuda.is_available() else "cpu")
 EPOCHS = 300
