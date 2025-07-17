528977476c8136b33acb29fdc34ca496de74270a
shree <singhishree@gmail.com>
2025-07-17 20:21:31 +0530

    path breakdown

--- compute_path.py
+++ compute_path.py
@@ -1,5 +1,5 @@
 
-def compute_path(latent_A, latent_B, model):
+def compute_path(latent_A, latent_B, model, n_steps=100):
     """
     Compute a path in latent space from latent_A to latent_B,
     while trying to keepmthe model output quantity constant along the path.
@@ -10,7 +10,7 @@ def compute_path(latent_A, latent_B, model):
     path = [current_latent]
     target_quantity = model(latent_B).detach().requires_grad_(False)
 
-    while True:
+    for steps_remaining in range(n_steps-1, 1, -1):
         current_quantity = model(current_latent)
         current_quantity.backward()
 
@@ -21,10 +21,9 @@ def compute_path(latent_A, latent_B, model):
         path_projection = remaining_path - (remaining_path@gradient / gradient_norm_squared) * gradient
         gradient_correction = (target_quantity - current_quantity) / gradient_norm_squared * gradient
         
-        current_latent = path_projection + gradient_correction
+        current_latent = path_projection / (steps_remaining+1) + gradient_correction
 
         path.append(current_latent.clone())
 
-        if (current_latent - latent_B).norm() < 1e-5:
-            break
+    path.append(latent_B)
     return path
